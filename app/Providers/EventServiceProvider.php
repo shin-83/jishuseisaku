1372c8d503c0ec8207217b544839bcf2364d73ae
shin-83 <145411891+shin-83@users.noreply.github.com>
2024-02-16 15:14:21 +0900

    管理者のみメニュー表示作成

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -6,6 +6,7 @@ use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
+use JeroenNoten\LaravelAdminLte\Events\BuildingMenu;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -26,6 +27,13 @@ class EventServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        Event::listen(BuildingMenu::class, function (BuildingMenu $event) {
+            if (auth()->user()->role == 0){
+                $event->menu->remove('(管理者用)商品一覧_admin_only');
+                $event->menu->remove('(管理者用)商品登録_admin_only');
+                $event->menu->remove('(管理者用)ユーザー管理_admin_only');
+            }
+        });
     }
 
     /**
